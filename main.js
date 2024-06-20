8959eb8d962eb9a06593953ace223ab64587c8f1
RiccardoMacchi <rick.mac97@outlook.com>
2024-06-20 17:28:53 +0200

    risolto counter like js aggiunta classe like con js

--- main.js
+++ main.js
@@ -63,38 +63,60 @@ posts.forEach((element) =>{
     console.log(element)
     container.innerHTML += 
     `<div class="post">
-    <div class="post__header">
-        <div class="post-meta">                    
-            <div class="post-meta__icon">
-                <img class="profile-pic" src="${element.author.image}" alt="${element.author.name}">                    
+        <div class="post__header">
+            <div class="post-meta">                    
+                <div class="post-meta__icon">
+                    <img class="profile-pic" src="${element.author.image}" alt="${element.author.name}">                    
+                </div>
+                <div class="post-meta__data">
+                    <div class="post-meta__author">${element.author.name}</div>
+                    <div class="post-meta__time">${element.created}</div>
+                </div>                    
             </div>
-            <div class="post-meta__data">
-                <div class="post-meta__author">${element.author.name}</div>
-                <div class="post-meta__time">${element.created}</div>
-            </div>                    
         </div>
-    </div>
-    <div class="post__text">${element.content}</div>
-    <div class="post__image">
-        <img src="${element.media}" alt="">
-    </div>
-    <div class="post__footer">
-        <div class="likes js-likes">
-            <div class="likes__cta">
-                <a class="like-button  js-like-button" href="#" data-postid="${element.id}">
-                    <i class="like-button__icon fas fa-thumbs-up" aria-hidden="true"></i>
-                    <span class="like-button__label">Mi Piace</span>
-                </a>
-            </div>
-            <div class="likes__counter">
-                Piace a <b id="like-counter-1" class="js-likes-counter">${element.likes}</b> persone
-            </div>
-        </div> 
-    </div>            
-</div>`
+        <div class="post__text">${element.content}</div>
+        <div class="post__image">
+            <img src="${element.media}" alt="">
+        </div>
+        <div class="post__footer">
+            <div class="likes js-likes">
+                <div class="likes__cta">
+                    <a class="like-button  js-like-button" href="#" data-postid="${element.id}">
+                        <i class="like-button__icon fas fa-thumbs-up" aria-hidden="true"></i>
+                        <span class="like-button__label">Mi Piace</span>
+                    </a>
+                </div>
+                <div class="likes__counter">
+                    Piace a <b id="like-counter-${element.id}" class="js-likes-counter">${element.likes}</b> persone
+                </div>
+            </div> 
+        </div>            
+    </div>`
+
 
 })
 
+
+// Arrey id like post
+const likeArrey = [];
 // Recupero bottone like
 
-let btnLike = document.querySelector(".js-like-button")
+let btnSLike = document.querySelectorAll(".js-like-button");
+let counters = document.querySelectorAll(".js-likes-counter")
+
+btnSLike.forEach((el, i)=>{
+    el.addEventListener("click",
+        (event)=> {
+            event.preventDefault();
+            const likeCounter = counters[i];
+            if (el.classList.contains("like-button--liked")){
+                posts[i].likes--;
+            } else {
+                posts[i].likes++;
+            }
+            el.classList.toggle("like-button--liked");
+            likeCounter.innerHTML = posts[i].likes;
+        }
+    )
+})
+
